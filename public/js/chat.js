f431f6a96a4a52029a4c46ae758acc0b2f0ceb0b
AlexisCast <castellanos.alexis@hotmail.com>
2023-08-29 20:45:49 -0700

    tracking users joining and leaving

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,9 +1,9 @@
 const socket = io();
 
-//Elements
+// Elements
 const $messageForm = document.querySelector("#message-form");
-const $messageFormInput = document.querySelector("input");
-const $messageFormButton = document.querySelector("button");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
 const $sendLocationButton = document.querySelector("#send-location");
 const $messages = document.querySelector("#messages");
 
@@ -28,7 +28,6 @@ socket.on("message", (message) => {
 });
 
 socket.on("locationMessage", (message) => {
-	console.log("locationMessage");
 	console.log(message);
 	const html = Mustache.render(locationMessageTemplate, {
 		url: message.url,
@@ -40,13 +39,11 @@ socket.on("locationMessage", (message) => {
 $messageForm.addEventListener("submit", (e) => {
 	e.preventDefault();
 
-	//disabled
 	$messageFormButton.setAttribute("disabled", "disabled");
 
 	const message = e.target.elements.message.value;
 
 	socket.emit("sendMessage", message, (error) => {
-		//enabled
 		$messageFormButton.removeAttribute("disabled");
 		$messageFormInput.value = "";
 		$messageFormInput.focus();
@@ -54,6 +51,7 @@ $messageForm.addEventListener("submit", (e) => {
 		if (error) {
 			return console.log(error);
 		}
+
 		console.log("Message delivered!");
 	});
 });
@@ -62,7 +60,7 @@ $sendLocationButton.addEventListener("click", () => {
 	if (!navigator.geolocation) {
 		return alert("Geolocation is not supported by your browser.");
 	}
-	//disable
+
 	$sendLocationButton.setAttribute("disabled", "disabled");
 
 	navigator.geolocation.getCurrentPosition((position) => {
@@ -73,7 +71,6 @@ $sendLocationButton.addEventListener("click", () => {
 				longitude: position.coords.longitude,
 			},
 			() => {
-				//enable
 				$sendLocationButton.removeAttribute("disabled");
 				console.log("Location shared!");
 			}
@@ -81,4 +78,9 @@ $sendLocationButton.addEventListener("click", () => {
 	});
 });
 
-socket.emit('join', { username, room })
+socket.emit("join", { username, room }, (error) => {
+	if (error) {
+		alert(error);
+		location.href = "/";
+	}
+});
--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ const {
 	generateMessage,
 	generateLocationMessage,
 } = require("./utils/messages");
+const {
+	addUser,
+	removeUser,
+	getUser,
+	getUsersInRoom,
+} = require("./utils/users");
 
 const app = express();
 const server = http.createServer(app);
@@ -21,29 +27,32 @@ app.use(express.static(publicDirectoryPath));
 io.on("connection", (socket) => {
 	console.log("New WebSocket connection");
 
-	socket.on("join", ({ username, room }) => {
-		socket.join(room);
+	socket.on("join", (options, callback) => {
+		const { error, user } = addUser({ id: socket.id, ...options });
+
+		if (error) {
+			return callback(error);
+		}
+
+		socket.join(user.room);
 
 		socket.emit("message", generateMessage("Welcome!"));
 		socket.broadcast
-			.to(room)
-			.emit("message", generateMessage(`${username} has joined!`));
+			.to(user.room)
+			.emit("message", generateMessage(`${user.username} has joined!`));
 
-		// socket.emit, io.emit, socket.broadcast.emit
-		// io.to.emit, socket.broadcast.to.emit
+		callback();
 	});
 
-	socket.broadcast.emit("message", generateMessage("A new user has joined!"));
-
-	socket.on("sendMessage", (message, calback) => {
+	socket.on("sendMessage", (message, callback) => {
 		const filter = new Filter();
 
 		if (filter.isProfane(message)) {
-			return calback("Profanity is not allowed!");
+			return callback("Profanity is not allowed!");
 		}
 
 		io.to("Center City").emit("message", generateMessage(message));
-		calback();
+		callback();
 	});
 
 	socket.on("sendLocation", (coords, callback) => {
@@ -57,7 +66,14 @@ io.on("connection", (socket) => {
 	});
 
 	socket.on("disconnect", () => {
-		io.emit("message", generateMessage("A user has left!"));
+		const user = removeUser(socket.id);
+
+		if (user) {
+			io.to(user.room).emit(
+				"message",
+				generateMessage(`${user.username} has left!`)
+			);
+		}
 	});
 });
 
